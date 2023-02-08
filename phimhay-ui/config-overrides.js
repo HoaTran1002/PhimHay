767c2b9d368951ed7f4893ec65e432592dab6ec1
moscow <vanhoa0908751520@gmail.com>
2023-02-09 01:45:16 +0700

    config babelrc

--- phimhay-ui/config-overrides.js
+++ phimhay-ui/config-overrides.js
@@ -1,7 +1,6 @@
 const {
-    override,
-    useBabelRc,
-  } = require("customize-cra");
-  const path = require("path");
-  
-  module.exports = override(useBabelRc());
+  override,
+  useBabelRc,
+} = require("customize-cra");
+
+module.exports = override(useBabelRc());
---
+++ phimhay-ui/src/components/GlobalStyles/index.js
@@ -0,0 +1,5 @@
+import "../GlobalStyles.scss"
+function GlobalStyles({chidlren}){
+    return chidlren
+}
+export default GlobalStyles;
