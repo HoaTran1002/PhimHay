c4567a27ad73a0e50b436edcb23debdb4d1ab3c4
moscow <vanhoa0908751520@gmail.com>
2023-02-26 10:05:58 +0700

    header editer

--- phimhay-ui/src/layout/Components/Header/index.js
+++ phimhay-ui/src/layout/Components/Header/index.js
@@ -1,18 +1,35 @@
 import classNames from 'classnames/bind';
 import styles from './Header.module.scss';
-import Images from '~/assets/Images';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faCircleXmark, faSpinner,faMagnifyingGlass} from '@fortawesome/free-solid-svg-icons';
+
+import Images from '~/assets/images';
 const cx = classNames.bind(styles);
 function Header() {
+
     return (
         <header className={cx('wrapper')}>
             <div className={cx('inner')}>
-                <div className={cx('logo')}>
-                    <img src={Images.logo} alt="Movies" />
+                <div className={cx('controll')}>
+                    <div classNames={cx('sidebar-btn')}>
+
+                    </div>
+                    <div className={cx('logo')} >
+                        <Images/>
+                    </div>
                 </div>
-                <div className={cx('navigation')}></div>
-                <div className={cx('search-block')}>
-                    <div className={cx('search-input')}></div>
-                    <div className={cx('search-button')}></div>
+                <div className={cx('search')}>
+                    <input placeholder='Search account and videos'/>
+                    <button className={cx('btn-clear')}> 
+                        <FontAwesomeIcon icon={faCircleXmark}/>
+                    </button>
+                    <FontAwesomeIcon className={cx('loading')} icon={faSpinner}/>
+                    <button className={cx('btn-search')}>
+                        <FontAwesomeIcon icon={faMagnifyingGlass}/>
+                    </button>
+                </div>
+                <div className={cx('right-header')}>
+
                 </div>
             </div>
         </header>
